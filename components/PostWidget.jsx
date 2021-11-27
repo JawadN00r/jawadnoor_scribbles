26b40aa17770f5aaf5c6f4f6601ce460e1669d1d
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-11-27 21:32:22 +0600

    postwidget ongoing

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
 const PostWidget = () => {
+  const [relatedPosts, setRelatedPosts] = useState([])
   return (
     <div>
       PostWidget
--- services/index.js
+++ services/index.js
@@ -33,3 +33,23 @@ export const getPosts = async () => {
   const result = await request(graphqlAPI, query);
   return result.postsConnection.edges;
 };
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      post(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
