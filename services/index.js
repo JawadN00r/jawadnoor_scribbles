fe43d2d190061265d20449312d76664394a1c6cd
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-11-28 16:56:12 +0600

    category added

--- services/index.js
+++ services/index.js
@@ -73,3 +73,16 @@ export const getSimilarPosts = async () => {
   const result = await request(graphqlAPI, query);
   return result.posts;
 };
+
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
+        name
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.categories;
+};
