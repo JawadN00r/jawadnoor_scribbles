d1e3ae17e9b2ccaebb21cd6cce875bb364583e17
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-11-28 15:05:57 +0600

    postwidget done

--- services/index.js
+++ services/index.js
@@ -37,8 +37,28 @@ export const getPosts = async () => {
 export const getRecentPosts = async () => {
   const query = gql`
     query GetPostDetails() {
-      post(
+      posts(
         orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories:[String!]) {
+      posts(
+        where { slug_not: $slug, AND: { categories_some: { slug_in: $categories }}}
         last: 3
       ) {
         title
