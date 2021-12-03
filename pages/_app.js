671544290d3bc072680b488b63f2732744db630b
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-12-04 00:30:23 +0600

    _document added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
