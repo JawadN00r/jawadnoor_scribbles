92cee0db9b19279fdf580893f4e232b00ad3c9c0
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-12-06 17:02:10 +0600

    category link error resolved ... no idea why

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,14 +1,14 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-import {getCategories} from '../services'
+import { getCategories } from '../services'
 
 const Categories = () => {
   const [categories, setCategories] = useState([])
   useEffect(() => {
     getCategories()
-    .then((newCategories)=>setCategories(newCategories))  
+      .then((newCategories) => setCategories(newCategories))
   }, [])
-  // console.log(categories)
+  console.log(categories)
   return (
     <div className="bg-white shadow-lg rounded-lg p-8
      pb-12 mb-8">
@@ -16,8 +16,9 @@ const Categories = () => {
        border-b pb-4">
         Categories
       </h3>
-      {categories.map((category)=>(
-        <Link key={category.slug} href={`/category/${category}`}>
+      {categories.map((category) => (
+        <Link key={category.slug}
+          href={`/category/${category.slug}`}>
           <span className="cursor-pointer block pb-3 mb-3">
             {category.name}
           </span>
