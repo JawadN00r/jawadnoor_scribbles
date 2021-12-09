6bbfd7637af61c8b7801b5f04ed41be0cc040a22
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-12-09 23:04:44 +0600

    margin and padding updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   }, [])
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+      <div className="border-b w-full inline-block border-blue-400 py-10">
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-semiboldbold text-2xl lg:text-4xl
