90fa45ddde3d249be619b942b3aa1aca0243c6ef
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2021-12-09 17:02:23 +0600

    previous next disable button updated

--- pages/index.js
+++ pages/index.js
@@ -43,14 +43,14 @@ export default function Home() {
             <PostCard post={post.node} key={post.node.title} />
           ))}
           <div className="flex justify-content absolute bottom-0 left-1/2 transform -translate-x-1/2 ">
-            <button areal-label="Previous" disabled="" className="hover:ring-2 hover:ring-offset-1 font-semibold 
+            <button areal-label="Previous" disabled={!seachedPosts.pageInfo.hasPreviousPage} className="hover:ring-2 hover:ring-offset-1 font-semibold 
             focus:ring-white focus:ring-2 focus:ring-offset-1 hover:ring-white
             focus:bg-black focus:outline-none hover:scale-95 w-full sm:w-auto 
             bg-black transition duration-150 ease-in-out rounded text-white
               px-8 py-3 text-sm mt-6 m-1 disabled:bg-gray-400 disabled:text-black">
                 Previous
             </button>
-            <button areal-label="Next" className="hover:ring-2 hover:ring-offset-1
+            <button areal-label="Next" disabled={!seachedPosts.pageInfo.hasNextPage} className="hover:ring-2 hover:ring-offset-1
               font-semibold hover:ring-white focus:ring-white focus:ring-2 
               focus:ring-offset-1 focus:bg-black focus:outline-none hover:scale-95 
               w-full sm:w-auto bg-black transition duration-150 ease-in-out rounded
