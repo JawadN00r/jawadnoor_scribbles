a51bc57b69a37f1e8ba44500663125b25a6fe7b7
Md Jawad Noor Asif <jawad.asif.bd@gmail.com>
2022-01-13 11:58:28 +0600

    Revert "mkdirsync removed for vercel deployment error"
    This reverts commit 698f3b2fff04694aa941b3a5ab76faa6f0ab1639.

--- pages/index.js
+++ pages/index.js
@@ -148,7 +148,7 @@ const generateRSSFeed = async()=>{
       date: new Date(post.createdAt),
     });
   });
-  // fs.mkdirSync("./public/rss", { recursive: true });
+  fs.mkdirSync("./public/rss", { recursive: true });
   fs.writeFileSync("./public/rss/feed.xml", feed.rss2());
   fs.writeFileSync("./public/rss/atom.xml", feed.atom1());
   fs.writeFileSync("./public/rss/feed.json", feed.json1());
